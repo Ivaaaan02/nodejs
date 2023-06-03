967284762034725da02330b3af4dd1fb1f604a19
Ivaaaan02 <ivanaldousvillafuerte2@gmail.com>
2023-06-03 09:41:18 +0800

    Initial Register

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 function Register() {
-    return <div>Register</div>;
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("form");
+    };
+    const handleChange = (event) => {};
+
+    return (
+    <>
+        <FormContainer >
+            <form onSubmit={(event) => handleSubmit(event)}>
+                <div className="brand">
+                    <img src="" alt="" />
+                    <h1>Ivan's Chat App</h1>
+                </div>
+                <input
+                    type="text"
+                    placeholder="Enter your Username"
+                    name="username"
+                    onChange={(e) => handleChange(e)}
+                />
+                <input
+                    type="email"
+                    placeholder="Enter your Email"
+                    name="email"
+                    onChange={(e) => handleChange(e)}
+                />
+                <input
+                    type="password"
+                    placeholder="Enter your Password"
+                    name="password"
+                    onChange={(e) => handleChange(e)}
+                />
+                <input
+                    type="password"
+                    placeholder="Confirm your Password"
+                    name="confirmPassword"
+                    onChange={(e) => handleChange(e)}
+                />
+            </form>
+            <button type="submit">Create Account</button>
+            <span>Already have an Account? <Link to="/login">Login</Link>
+            </span>
+        </FormContainer>
+    </>
+    );
 }
+
+const FormContainer = styled.div``;
 
 export default Register;
